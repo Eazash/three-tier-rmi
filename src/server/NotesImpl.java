cbdcb3169466d3f578e5893caa2a9f9238b72f74
Ezra Ashenafi <eazash22@gmail.com>
2021-05-31 18:20:14 +0300

    create base classes for Server implementations

---
+++ src/server/NotesImpl.java
@@ -0,0 +1,5 @@
+package server;
+
+public class NotesImpl implements NotesInterface{
+    //function definitions for remote functions for working with notes
+}
---
+++ src/server/NotesInterface.java
@@ -0,0 +1,7 @@
+package server;
+
+import java.rmi.Remote;
+
+public interface NotesInterface extends Remote {
+    //function declaration for remote note manipulating functions
+}
